132f964c4c78cdd75c5136ea8a7c91f2b8e160b6
kll3rbee <32694575+kll3rbee@users.noreply.github.com>
2017-11-30 22:54:42 +0800

    com

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,20 @@
 import media
-john_wick = media.Movie ("John Wick", "Don't set him off.", "https://upload.wikimedia.org/wikipedia/en/9/98/John_Wick_TeaserPoster.jpg", "https://www.youtube.com/watch?v=2AUmvWm5ZDQ")
+import fresh_tomatoes
+#john_wick = media.Movie ("John Wick", "Don't set him off.", "https://upload.wikimedia.org/wikipedia/en/9/98/John_Wick_TeaserPoster.jpg", "https://www.youtube.com/watch?v=2AUmvWm5ZDQ")
 
-print (john_wick.storyline)
-john_wick.show_trailer()
+#print (john_wick.storyline)
+#john_wick.show_trailer()
+toy_story = media.Movie("Toy Story","A story of a boy and his boys that come to life","http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie("Avatar","A marine on an alien planet","http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg","http://www.youtube.com/watch?v=-9ceBgWV8io")
+
+school_of_rock = media.Movie("School of Rock", "Storyline","http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg","https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille","Storyline","http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg","https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Storyline","http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg","https://www.youtube.com/watch?v=atLg2wQQxvU")
+
+hunger_games = media.Movie("Hunger Games","Storyline","http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg","https://www.youtube.com/watch?v=Pb63a7H0bo")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris,hunger_games]
+fresh_tomatoes.open_movies_page(movies)
