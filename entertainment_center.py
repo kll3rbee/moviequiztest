64dbc4d7f1ac0595803f2b7945c6eeaeb47ddd57
kll3rbee <32694575+kll3rbee@users.noreply.github.com>
2017-11-30 17:51:48 +0800

    update

---
+++ entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+john_wick = media.Movie ("John Wick", "Don't set him off.", "https://upload.wikimedia.org/wikipedia/en/9/98/John_Wick_TeaserPoster.jpg", "https://www.youtube.com/watch?v=2AUmvWm5ZDQ")
+
+print (john_wick.storyline)
+john_wick.show_trailer()
